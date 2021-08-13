d700e167b4c6172d3ca77d1575ae1bf50bf7031d
Isaiah Haley <isaiah.haley@ucdenver.edu>
2021-08-13 23:30:26 +0100

    formatting updates

--- app.py
+++ app.py
@@ -11,4 +11,5 @@ def index():
     return render_template('home.html', temperature = test.get_temp())
 
 if __name__ == '__main__':
+    test.get_storage()
     app.run(debug=True, port=5000, host='0.0.0.0')
--- monitor.py
+++ monitor.py
@@ -11,6 +11,13 @@ class Monitor:
         temp = temp.split('\'')[0]
         return temp
 
+    def get_storage(self):
+        stor = subprocess.run(["df"], stdout=subprocess.PIPE)
+        stor = str(stor.stdout).split("\\n")
+        for l in stor:
+            print(l)
     
+    def get_cpu_usage(self):
+        return 0
 
         
