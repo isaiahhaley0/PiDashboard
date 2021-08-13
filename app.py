8096fcc9cb24dc992f3214eec2155ff2a27f8c2e
Isaiah Haley <isaiah.haley@ucdenver.edu>
2021-08-13 21:48:56 +0100

    Templates Added

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
  
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return 'Hello World'
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
