e12b2069ff2572d6ee2d30d0a8b59dfe55d78ffc
Isaiah Haley <isaiah.haley@ucdenver.edu>
2021-08-14 02:31:49 +0100

    set up Vue app request

--- static/js/monitorApp.js
+++ static/js/monitorApp.js
@@ -1,6 +1,22 @@
 const app = Vue.createApp({
     data() {
-        return { memory: 5}
+        return { memory: 5,
+                temperature: 0}
     },
-    delimiters: ["[[","]]"]
+    delimiters: ["[[","]]"],
+    mounted(){
+        this.getTemp();
+    },
+    methods:{
+        getTemp(){
+            fetch("/temperature").then(
+                response=>response.json()
+                ).then(
+                       temperature=>(this.temperature=temperature["temperature"])
+                        //probably the dumbest way of doing this
+                );
+     
+        }
+            
+}
 }).mount("#monitor")
